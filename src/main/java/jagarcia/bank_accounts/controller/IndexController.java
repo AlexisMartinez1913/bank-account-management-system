6c25d25e86ee68d14367795cb2a5cdd0091239e7
Alexis Martinez <jhonyalexis100@gmail.com>
2025-01-02 15:16:10 -0500

    index controller with primefaces/jsf

---
+++ src/main/java/jagarcia/bank_accounts/controller/IndexController.java
@@ -0,0 +1,38 @@
+package jagarcia.bank_accounts.controller;
+
+import jagarcia.bank_accounts.model.Account;
+import jagarcia.bank_accounts.service.impl.AccountService;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Data
+@ViewScoped
+public class IndexController {
+    @Autowired
+    AccountService accountService;
+
+    private List<Account> accounts;
+    //mandar info a la consola
+    private  static final Logger logger =
+            LoggerFactory.getLogger(IndexController.class);
+    @PostConstruct
+    public void init() {
+        loadData();
+    }
+
+    //traer datos
+    public void loadData() {
+        this.accounts = accountService.getAllAccounts();
+        accounts.forEach((account -> logger.info(account.toString())));
+    }
+
+
+}
