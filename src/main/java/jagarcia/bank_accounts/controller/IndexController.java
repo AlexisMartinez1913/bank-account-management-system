31cf47406ddfd5c32d01cd28030618e2ee13c38e
Alexis Martinez <jhonyalexis100@gmail.com>
2025-01-12 18:24:54 -0500

    to finish add acounts with primefaces

--- src/main/java/jagarcia/bank_accounts/controller/IndexController.java
+++ src/main/java/jagarcia/bank_accounts/controller/IndexController.java
@@ -3,8 +3,11 @@ package jagarcia.bank_accounts.controller;
 import jagarcia.bank_accounts.model.Account;
 import jagarcia.bank_accounts.service.impl.AccountService;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +23,7 @@ public class IndexController {
     AccountService accountService;
 
     private List<Account> accounts;
-    //atributo para cargar cuenta especifica
+    //atributo para cargar y crear cuenta especifica
     private Account selectedAccount;
 
     //mandar info a la consola
@@ -40,6 +43,21 @@ public class IndexController {
     public void addAccount() {
         this.selectedAccount = new Account();
 
+    }
+
+    public void saveAccount() {
+        logger.info("Account to save:" + this.selectedAccount);
+        if (this.selectedAccount.getIdAccount() == null) {
+            this.accountService.saveAccount(this.selectedAccount);
+            this.accounts.add(this.selectedAccount);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Account Added!"));
+        }
+        //ocultar vendana
+        PrimeFaces.current().executeScript("PF('windowModalAccount').hide()");
+        //actualizar tabla
+        PrimeFaces.current().ajax().update("form-accounts:messages",
+                "form-accounts:table-accounts");
     }
 
 
